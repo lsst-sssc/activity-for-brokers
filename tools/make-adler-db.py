ab0e9ac917601c4f83dd906a38a4f62e36223384
Michael S. P. Kelley <msk@astro.umd.edu>
2026-02-20 06:59:46 -0500

    Final working script

--- tools/make-adler-db.py
+++ tools/make-adler-db.py
@@ -1,26 +1,167 @@
 """
 Make an Adler database from real light curves.
 
+Warning:
+
+    * The reference frame has not been checked.
+    * The time scales have not been checked.
+    * This script will overwrite existing data in the database.
+    * ATLAS c and o are renamed g and r.
+
 See data/README.md for input data format.
 
+
+  target     ssObjectId
+--------- ----------------
+      19P 6592633514653240
+      46P 2358867111535997
+      67P  940303576399572
+      97P 8285973462698227
+      99P 1290030297348764
+     104P 5113060619462226
+     117P 3575262679851537
+     118P   69165524166199
+     179P 2462443667160976
+     285P 3138720172365267
+     382P  436637127094172
+     459P 3511020769536744
+C/2021 S3  798904090225145
+C/2021 Y1 1709597069217518
+     6478 1890843518253925
+
+
 Output format based on DP1:
-
-SSSource columns
-Index(['diaSourceId', 'eclipticBeta', 'eclipticLambda', 'galacticB',
-       'galacticL', 'heliocentricDist', 'heliocentricVX', 'heliocentricVY',
-       'heliocentricVZ', 'heliocentricX', 'heliocentricY', 'heliocentricZ',
-       'phaseAngle', 'residualDec', 'residualRa', 'ssObjectId',
-       'topocentricDist', 'topocentricVX', 'topocentricVY', 'topocentricVZ',
-       'topocentricX', 'topocentricY', 'topocentricZ'],
-      dtype='object')
-
-SSObject columns
-Index(['discoverySubmissionDate', 'numObs', 'ssObjectId'], dtype='object')
-
-MPCOrb columns
-Index(['e', 'epoch', 'incl', 'mpcDesignation', 'mpcH', 'node', 'peri', 'q',
-       'ssObjectId', 't_p'],
-      dtype='object')
+CREATE TABLE IF NOT EXISTS "DiaSource" (
+"apFlux" REAL,
+  "apFlux_flag" INTEGER,
+  "apFlux_flag_apertureTruncated" INTEGER,
+  "apFluxErr" REAL,
+  "band" TEXT,
+  "bboxSize" INTEGER,
+  "centroid_flag" INTEGER,
+  "coord_dec" REAL,
+  "coord_ra" REAL,
+  "dec" REAL,
+  "decErr" REAL,
+  "detector" INTEGER,
+  "diaObjectId" INTEGER,
+  "diaSourceId" INTEGER,
+  "dipoleAngle" REAL,
+  "dipoleChi2" REAL,
+  "dipoleFitAttempted" INTEGER,
+  "dipoleFluxDiff" REAL,
+  "dipoleFluxDiffErr" REAL,
+  "dipoleLength" REAL,
+  "dipoleMeanFlux" REAL,
+  "dipoleMeanFluxErr" REAL,
+  "dipoleNdata" INTEGER,
+  "extendedness" REAL,
+  "forced_PsfFlux_flag" INTEGER,
+  "forced_PsfFlux_flag_edge" INTEGER,
+  "forced_PsfFlux_flag_noGoodPixels" INTEGER,
+  "isDipole" INTEGER,
+  "ixx" REAL,
+  "ixxPSF" REAL,
+  "ixy" REAL,
+  "ixyPSF" REAL,
+  "iyy" REAL,
+  "iyyPSF" REAL,
+  "midpointMjdTai" REAL,
+  "parentDiaSourceId" INTEGER,
+  "pixelFlags" INTEGER,
+  "pixelFlags_bad" INTEGER,
+  "pixelFlags_cr" INTEGER,
+  "pixelFlags_crCenter" INTEGER,
+  "pixelFlags_edge" INTEGER,
+  "pixelFlags_injected" INTEGER,
+  "pixelFlags_injected_template" INTEGER,
+  "pixelFlags_injected_templateCenter" INTEGER,
+  "pixelFlags_injectedCenter" INTEGER,
+  "pixelFlags_interpolated" INTEGER,
+  "pixelFlags_interpolatedCenter" INTEGER,
+  "pixelFlags_nodata" INTEGER,
+  "pixelFlags_nodataCenter" INTEGER,
+  "pixelFlags_offimage" INTEGER,
+  "pixelFlags_saturated" INTEGER,
+  "pixelFlags_saturatedCenter" INTEGER,
+  "pixelFlags_streak" INTEGER,
+  "pixelFlags_streakCenter" INTEGER,
+  "pixelFlags_suspect" INTEGER,
+  "pixelFlags_suspectCenter" INTEGER,
+  "psfChi2" REAL,
+  "psfFlux" REAL,
+  "psfFlux_flag" INTEGER,
+  "psfFlux_flag_edge" INTEGER,
+  "psfFlux_flag_noGoodPixels" INTEGER,
+  "psfFluxErr" REAL,
+  "psfNdata" INTEGER,
+  "ra" REAL,
+  "ra_dec_Cov" REAL,
+  "raErr" REAL,
+  "reliability" REAL,
+  "scienceFlux" REAL,
+  "scienceFluxErr" REAL,
+  "shape_flag" INTEGER,
+  "shape_flag_no_pixels" INTEGER,
+  "shape_flag_not_contained" INTEGER,
+  "shape_flag_parent_source" INTEGER,
+  "snr" REAL,
+  "ssObjectId" INTEGER,
+  "time_processed" TEXT,
+  "trail_flag_edge" INTEGER,
+  "trailAngle" REAL,
+  "trailDec" REAL,
+  "trailFlux" REAL,
+  "trailLength" REAL,
+  "trailRa" REAL,
+  "visit" INTEGER,
+  "x" REAL,
+  "xErr" REAL,
+  "y" REAL,
+  "yErr" REAL
+);
+CREATE TABLE IF NOT EXISTS "SSSource" (
+"diaSourceId" INTEGER,
+  "eclipticBeta" REAL,
+  "eclipticLambda" REAL,
+  "galacticB" REAL,
+  "galacticL" REAL,
+  "heliocentricDist" REAL,
+  "heliocentricVX" REAL,
+  "heliocentricVY" REAL,
+  "heliocentricVZ" REAL,
+  "heliocentricX" REAL,
+  "heliocentricY" REAL,
+  "heliocentricZ" REAL,
+  "phaseAngle" REAL,
+  "residualDec" REAL,
+  "residualRa" REAL,
+  "ssObjectId" INTEGER,
+  "topocentricDist" REAL,
+  "topocentricVX" REAL,
+  "topocentricVY" REAL,
+  "topocentricVZ" REAL,
+  "topocentricX" REAL,
+  "topocentricY" REAL,
+  "topocentricZ" REAL
+);
+CREATE TABLE IF NOT EXISTS "SSObject" (
+"discoverySubmissionDate" REAL,
+  "numObs" INTEGER,
+  "ssObjectId" INTEGER
+);
+CREATE TABLE IF NOT EXISTS "MPCORB" (
+"e" REAL,
+  "epoch" REAL,
+  "incl" REAL,
+  "mpcDesignation" TEXT,
+  "mpcH" REAL,
+  "node" REAL,
+  "peri" REAL,
+  "q" REAL,
+  "ssObjectId" INTEGER,
+  "t_p" REAL
+);
 
 """
 
@@ -30,8 +171,11 @@ import sqlite3
 import argparse
 
 import numpy as np
+import pandas as pd
+import astropy.units as u
 from astropy.io import ascii
 from astropy.time import Time
+from astropy.table import Table, vstack
 from astroquery.jplhorizons import Horizons
 from sbpy.data import Names
 
@@ -51,29 +195,113 @@ targets = {
     "459P": "459P_ATLAS.csv",
     "C/2021 S3": "C2021S3_LOOK.csv",
     "C/2021 Y1": "C2021Y1_LOOK.csv",
-    "7968": "7968_ZTF_FINK.csv",
-    "2016 UU121": "2016UU121_ZTF_FINK.csv",
-    "2060": "Chiron_ZTF_FINK.csv",
-    "60558": "Echeclus_ZTF_FINK.csv",
+    # "7968": "7968_ZTF_FINK.csv",  # FINK tables are missing filter
+    # "2016 UU121": "2016UU121_ZTF_FINK.csv",
+    # "2060": "Chiron_ZTF_FINK.csv",
+    # "60558": "Echeclus_ZTF_FINK.csv",
     "6478": "Gault_ZTF.csv",
-    # "C/Hy model": "LPC_y3.csv",
-    # "C/Hab model": "LPC_0.3a-1b.csv",
+}
+
+# replacement filter names (for ATLAS)
+filter = {
+    "c": "g",
+    "o": "r",
 }
 
 
 def to_id(s):
-    """Convert a string to stable ID (integer) value using an MD5 hash.
-
-    The hash is trimmed to 8 digits.  Cross your fingers for no collisions!
-
-    """
+    """Convert a string to stable ID (integer) value using an MD5 hash."""
 
     hash = hashlib.md5()
     hash.update(s.encode())
-    return int.from_bytes(hash.digest()) % 10**8
+    return int.from_bytes(hash.digest()) % 10**16
 
 
-def make_object(target, num_obs):
+diasource_columns = [
+    "apFlux_flag",
+    "apFlux_flag_apertureTruncated",
+    "bboxSize",
+    "centroid_flag",
+    "coord_dec",
+    "coord_ra",
+    "dec",
+    "decErr",
+    "detector",
+    "diaObjectId",
+    "dipoleAngle",
+    "dipoleChi2",
+    "dipoleFitAttempted",
+    "dipoleFluxDiff",
+    "dipoleFluxDiffErr",
+    "dipoleLength",
+    "dipoleMeanFlux",
+    "dipoleMeanFluxErr",
+    "dipoleNdata",
+    "extendedness",
+    "forced_PsfFlux_flag",
+    "forced_PsfFlux_flag_edge",
+    "forced_PsfFlux_flag_noGoodPixels",
+    "isDipole",
+    "ixx",
+    "ixxPSF",
+    "ixy",
+    "ixyPSF",
+    "iyy",
+    "iyyPSF",
+    "parentDiaSourceId",
+    "pixelFlags",
+    "pixelFlags_bad",
+    "pixelFlags_cr",
+    "pixelFlags_crCenter",
+    "pixelFlags_edge",
+    "pixelFlags_injected",
+    "pixelFlags_injected_template",
+    "pixelFlags_injected_templateCenter",
+    "pixelFlags_injectedCenter",
+    "pixelFlags_interpolated",
+    "pixelFlags_interpolatedCenter",
+    "pixelFlags_nodata",
+    "pixelFlags_nodataCenter",
+    "pixelFlags_offimage",
+    "pixelFlags_saturated",
+    "pixelFlags_saturatedCenter",
+    "pixelFlags_streak",
+    "pixelFlags_streakCenter",
+    "pixelFlags_suspect",
+    "pixelFlags_suspectCenter",
+    "psfChi2",
+    "psfFlux",
+    "psfFlux_flag",
+    "psfFlux_flag_edge",
+    "psfFlux_flag_noGoodPixels",
+    "psfFluxErr",
+    "psfNdata",
+    "ra",
+    "ra_dec_Cov",
+    "raErr",
+    "reliability",
+    "scienceFlux",
+    "scienceFluxErr",
+    "shape_flag",
+    "shape_flag_no_pixels",
+    "shape_flag_not_contained",
+    "shape_flag_parent_source",
+    "time_processed",
+    "trail_flag_edge",
+    "trailAngle",
+    "trailDec",
+    "trailFlux",
+    "trailLength",
+    "trailRa",
+    "visit",
+    "x",
+    "xErr",
+    "y",
+    "yErr",
+]
+
+
+def make_ssobject(target, num_obs):
     return {
         "discoverySubmissionDate": 60000.0,
         "numObs": num_obs,
@@ -87,62 +315,134 @@ def get_ephemeris(target, mjd):
     if Names.asteroid_or_comet(target) or target.startswith("A/"):
         opts = dict(no_fragments=True, closest_apparition=True)
 
-    eph = None
-    orbit = None
-    for _mjd in np.array_split(mjd, len(mjd) // 100):
+    helio = []
+    topo = []
+    for _mjd in np.array_split(mjd, len(mjd) // 50):
         q = Horizons(
             target,
-            id_type="designation",
+            id_type=None if target.isdigit() else "designation",
             epochs=_mjd,
-            location="X05",
+            location="@sun",
         )
-        _eph = q.ephemerides(**opts)
+        helio.append(q.vectors(**opts))
 
-        if eph is None:
-            eph = _eph
-        else:
-            eph.add_rows(_eph)
+        q.location = "X05"
+        topo.append(q.vectors(**opts))
 
-    q.epochs = [np.mean(mjd)]
-    orbit = q.elements(**opts)
+    helio = vstack(helio)
+    topo = vstack(topo)
 
-    return eph, orbit
+    return helio, topo
 
 
-def make_sources(target, ssobject, data):
-    eph, orbit = get_ephemeris(target, data["mjd"])
-    breakpoint()
-    rows = []
-    for row in data:
-        rows.append(
+def phase_angle(helio, topo):
+    # sun->target
+    rs = np.array(
+        [
+            helio["x"],
+            helio["y"],
+            helio["z"],
+        ]
+    )
+
+    # observer->target
+    ro = np.array(
+        [
+            topo["x"],
+            topo["y"],
+            topo["z"],
+        ]
+    )
+
+    # phase angle
+    phase = np.degrees(np.arccos(np.dot(rs, ro) / helio["range"] / topo["range"]))
+
+    return phase
+
+
+def make_source(target, ssobject, data):
+    helio, topo = get_ephemeris(target, data["mjd"])
+    sssource = []
+    diasource = []
+    for i, row in enumerate(data):
+        diaSourceId = to_id(str(row))
+        apflux = (row["mag"] * u.ABmag).to_value(u.nJy)
+        diasource.append(
             {
-                "diaSourceId": to_id(str(row)),
-                "eclipticBeta": 0,
-                "eclipticLambda": 0,
-                "galacticB": 0,
-                "galacticL": 0,
-                "heliocentricDist": 0,
-                "heliocentricVX": 0,
-                "heliocentricVY": 0,
-                "heliocentricVZ": 0,
-                "heliocentricX": 0,
-                "heliocentricY": 0,
-                "heliocentricZ": 0,
-                "phaseAngle": 0,
-                "residualDec": 0,
-                "residualRa": 0,
+                "apFlux": apflux,
+                "apFluxErr": row["mag_err"] * apflux * 1.0857,
+                "band": filter.get(row["filter"], row["filter"]),
+                "midpointMjdTai": Time(row["mjd"], format="mjd", scale="utc").tai.mjd,
                 "ssObjectId": ssobject["ssObjectId"],
-                "topocentricDist": 0,
-                "topocentricVX": 0,
-                "topocentricVY": 0,
-                "topocentricVZ": 0,
-                "topocentricX": 0,
-                "topocentricY": 0,
-                "topocentricZ": 0,
+                "diaSourceId": diaSourceId,
             }
         )
 
-    return rows
+        for col in diasource_columns:
+            diasource[-1][col] = np.nan
+
+        sssource.append(
+            {
+                "diaSourceId": diaSourceId,
+                "eclipticBeta": np.nan,
+                "eclipticLambda": np.nan,
+                "galacticB": np.nan,
+                "galacticL": np.nan,
+                "heliocentricDist": helio["range"][i],
+                "heliocentricVX": helio["vx"][i],
+                "heliocentricVY": helio["vy"][i],
+                "heliocentricVZ": helio["vz"][i],
+                "heliocentricX": helio["x"][i],
+                "heliocentricY": helio["y"][i],
+                "heliocentricZ": helio["z"][i],
+                "phaseAngle": phase_angle(helio[i], topo[i]),
+                "residualDec": np.nan,
+                "residualRa": np.nan,
+                "ssObjectId": ssobject["ssObjectId"],
+                "topocentricDist": topo["range"][i],
+                "topocentricVX": topo["vx"][i],
+                "topocentricVY": topo["vy"][i],
+                "topocentricVZ": topo["vz"][i],
+                "topocentricX": topo["x"][i],
+                "topocentricY": topo["y"][i],
+                "topocentricZ": topo["z"][i],
+            }
+        )
+
+    return diasource, sssource
+
+
+def get_orbit(target, mjd):
+    # use comet options for comets and comet-like orbits
+    opts = {}
+    if Names.asteroid_or_comet(target) or target.startswith("A/"):
+        opts = dict(no_fragments=True, closest_apparition=True)
+
+    q = Horizons(
+        target,
+        id_type=None if target.isdigit() else "designation",
+        epochs=[np.mean(mjd)],
+        location="@sun",
+    )
+    orbit = q.elements(**opts)
+    return orbit
+
+
+def make_orbit(target, data):
+    orbit = get_orbit(target, data["mjd"])
+    H = "M1" if "M1" in orbit.colnames else "H"
+    return {
+        "e": orbit["e"][0],
+        "epoch": Time(orbit["datetime_jd"][0], format="jd").mjd,
+        "incl": orbit["incl"][0],
+        "mpcDesignation": target,
+        "mpcH": orbit[H][0],
+        "node": orbit["Omega"][0],
+        "peri": orbit["w"][0],
+        "q": orbit["q"][0],
+        "ssObjectId": to_id(target),
+        "t_p": Time(orbit["Tp_jd"][0], format="jd").mjd,
+    }
 
 
 def main():
@@ -157,26 +457,35 @@ def main():
 
     db = sqlite3.connect(args.filename)
 
-    sssources = []
-    ssobjects = []
+    diasource = []
+    sssource = []
+    ssobject = []
+    mpcorb = []
+    target_table = []
     for target, fn in targets.items():
+        print(target)
         data = ascii.read(os.path.join(args.path, fn))
 
         # create the data tables
-        ssobject = make_object(target, len(data))
-        sssource = make_sources(target, ssobject, data)
+        ssobject.append(make_ssobject(target, len(data)))
+        source = make_source(target, ssobject[-1], data)
+        diasource.extend(source[0])
+        sssource.extend(source[1])
+        mpcorb.append(make_orbit(target, data))
 
-        # concatenate the data
-        ssobjects.append(ssobject)
-        sssources.extend(sssource)
+        target_table.append(
+            {"target": target, "ssObjectId": ssobject[-1]["ssObjectId"]}
+        )
 
-        break
+    pd.DataFrame(ssobject).to_sql("SSObject", con=db, if_exists="replace", index=False)
+    pd.DataFrame(diasource).to_sql(
+        "DiaSource", con=db, if_exists="replace", index=False
+    )
+    pd.DataFrame(sssource).to_sql("SSSource", con=db, if_exists="replace", index=False)
+    pd.DataFrame(mpcorb).to_sql("MPCOrb", con=db, if_exists="replace", index=False)
 
-    # ssobject.to_sql("SSObject", con=db, if_exists="replace", index=False)
-    # sssource.to_sql("SSSource", con=db, if_exists="replace", index=False)
-
-    return data, ssobject, sssource
+    Table(target_table).pprint_all()
 
 
 if __name__ == "__main__":
-    data, ssobject, sssource = main()
+    main()
